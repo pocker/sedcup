3c55f6fd5763b1404f89376d23cf67e1078d9c08
pocker <szdavee@gmail.com>
2016-01-29 15:49:29 +0100

    no message

--- src/main/webapp/js/controller/mapController.js
+++ src/main/webapp/js/controller/mapController.js
@@ -1,23 +1,53 @@
 /**
  * 
  */
+
 (function() {
 	'use strict';
 	angular.module('sedcup').controller('mapController', mapController);
 
+	var pois = [
+			[ '1', 'Rongy kocsma', '46.263883', '20.131972' ],
+			[ '2', 'Havanna söröző', '46.252753', '20.132720' ],
+			[ '3', 'Pivo vár', '46.247843', '20.142431' ],
+			[ '4', 'Soccer club', '46.252578', '20.143465' ],
+			[ '5', 'Hungi vigadó', '46.254485', '20.152984' ],
+			[ '6', 'Vak egér', '46.245715', '20.159241' ],
+			[ '7', 'Betyárok tanyája', '46.258182', '20.168439' ],
+			[ '8', 'Blues cafe', '46.250265', '20.147396' ],
+			[ '9', 'Acapella', '46.252011', '20.147569' ],
+			[ '10', 'Cairo cafe', '46.251567', '20.149837' ],
+			[ '11', 'McDonalds', '46.251365', '20.146798' ],
+			[ '12', 'Kárász utca pad', '46.251121', '20.147405' ],
+			[ '13', 'Tesco Express Dugonics tér', '46.250791', '20.143704' ],
+			[ '14', 'Tesco Express Tisza Lajos sgt.', '46.256206', '20.148070' ],
+			[ '15', 'Tesco Tisza Center', '46.259499', '20.171159' ],
+			[ '16', 'Széchenyi tér pad', '46.254493', '20.149307' ],
+			[ '17', 'Tisza part', '46.250175', '20.151811' ] ];
+
 	function mapController($scope) {
+
+		var markerArray = [];
+
+		var markers = [];
 
 		var map = new google.maps.Map(document.getElementById('map'), {
 			center : {
-				lat : -34.397,
-				lng : 150.644
+				lat : 46.250,
+				lng : 20.146
 			},
 			disableDefaultUI : true,
-			zoom : 10
+			scrollwheel : false,
+			navigationControl : false,
+			mapTypeControl : false,
+			scaleControl : false,
+			draggable : false,
+			zoom : 13
 		});
 		var marker = new google.maps.Marker({
 			map : map,
-			draggable : true,
+			draggable : false,
+			icon : 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
 			animation : google.maps.Animation.DROP,
 			position : {
 				lat : 59.327,
@@ -25,6 +55,33 @@
 			}
 		});
 		marker.addListener('click', toggleBounce);
+
+		
+		addMarker(0);
+		function addMarker(i){
+			
+			var marker = new google.maps.Marker(
+					{
+						map : map,
+						draggable : false,
+						icon : 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+						animation : google.maps.Animation.DROP,
+						position : {
+							lat : parseFloat(pois[i][2]),
+							lng : parseFloat(pois[i][3])
+						}
+					});
+	
+			markers.push(marker);
+			i++;
+			if(i<pois.length){
+				setTimeout(function(){addMarker(i)}, 100);
+			}
+		}
+
+		function setMapOn(marker, map) {
+			marker.setMap(map);
+		}
 
 		function toggleBounce() {
 			if (marker.getAnimation() !== null) {
